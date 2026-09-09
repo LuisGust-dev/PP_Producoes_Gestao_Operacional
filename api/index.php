fa7635877f75ec5c30c368fd37ee9235775f7921
LuisGust-dev <dev.gulcs@gmail.com>
2026-09-09 19:21:30 -0300

    Prepara diretorios temporarios na Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
+$tmpDirectories = [
+    '/tmp/views',
+    '/tmp/cache',
+    '/tmp/sessions',
+];
+
+foreach ($tmpDirectories as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0777, true);
+    }
+}
+
 require __DIR__.'/../public/index.php';
